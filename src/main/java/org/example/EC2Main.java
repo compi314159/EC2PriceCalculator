48a5e656c17130c0ddbe5e894366585c5a164f38
compi314159 <71290813+compi314159@users.noreply.github.com>
2023-06-22 18:54:24 -0500

    Upload version 2
    Price retrieved from database using jedis
    Price retrieved from AWS if not already in database

--- src/main/java/org/example/EC2Main.java
+++ src/main/java/org/example/EC2Main.java
@@ -1,18 +1,28 @@
 package org.example;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
-
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 import software.amazon.awssdk.services.pricing.PricingClient;
 import software.amazon.awssdk.services.pricing.model.*;
+import redis.clients.jedis.Jedis;
 
 public class EC2Main {
     public static void main(String[] args) throws MalformedURLException {
-        Scanner input = new Scanner(System.in);
-        ProfileCredentialsProvider credentialsProvider = ProfileCredentialsProvider.create();
+        // connect to jedis
+        Jedis jedis = new Jedis("redis://default:8GICzgZMnQdshTOrtW6DMOyWgosU6Ng7@redis-17504.c91.us-east-1-3.ec2.cloud.redislabs.com:17504");
+        jedis.connect();
 
+        // check if connection is successful
+        if(jedis.ping().equalsIgnoreCase("pong"))
+        {
+            System.out.println("Connection successful");
+        }
+
+        Scanner input = new Scanner(System.in); // import scanner
+
+        ProfileCredentialsProvider credentialsProvider = ProfileCredentialsProvider.create();
         PricingClient pricingClient = PricingClient.builder().region(Region.US_EAST_1)
                 .credentialsProvider(credentialsProvider).build();
 
@@ -21,29 +31,32 @@ public class EC2Main {
         while(cont.equalsIgnoreCase("y")) {
             System.out.print("\nEnter EC2 instance type: ");
             String instanceType = input.nextLine().trim();
-            try {
-
-                GetProductsRequest request = GetProductsRequest.builder().serviceCode("AmazonEC2")
-                        .filters(Filter.builder().field("instanceType").value(instanceType)
-                                .type("TERM_MATCH").build()).maxResults(1).build();
-
-                GetProductsResponse response = pricingClient.getProducts(request);
-                // Get price
-                String resp = response.toString();
-                String terms = resp.substring(resp.indexOf("terms"));
-                String price = terms.substring(terms.indexOf("pricePerUnit"));
-                System.out.println("--> " + instanceType + " price per hour: " + price.substring(14, price.indexOf("}") + 1));
+            try
+            {
+                String price;
+                if(!(getAWSCost(jedis, instanceType) == null))
+                {
+                    price = getAWSCost(jedis, instanceType);
+                    System.out.println(instanceType + " successfully found in database!");
+                }
+                else {
+                    System.out.println(instanceType + " not found in database. Receiving from aws");
+                    price = getInstantAWSCost(instanceType, credentialsProvider, pricingClient, input);
+                    addAWSCost(jedis, instanceType, price);
+                }
+                System.out.println("--> " + instanceType + " price per hour: " + price);
                 System.out.print("Continue? (y/n): ");
                 cont = input.nextLine();
 
-            } catch (StringIndexOutOfBoundsException e) {
+            }
+            catch (StringIndexOutOfBoundsException e)
+            {
                 System.out.println("Error: " + e);
                 URL EC2Types = new URL("https://aws.amazon.com/ec2/instance-types/");
                 System.out.print("Please enter valid instance type (see " + EC2Types + "), try again? (y/n): ");
                 cont = input.nextLine();
             }
-            catch (Exception e)
-            {
+            catch (Exception e) {
                 System.out.println("Error: " + e);
                 System.out.print("Invalid entry, try again? (y/n): ");
                 cont = input.nextLine();
@@ -51,5 +64,30 @@ public class EC2Main {
         }
     }
 
+    // add cost to database
+    private static void addAWSCost(Jedis jedis, String instanceType, String price) {
+        jedis.set(instanceType, price);
+    }
+
+    // get cost from database
+    public static String getAWSCost(Jedis jedis, String instanceType)
+    {
+        return jedis.get(instanceType);
+    }
+
+    // get cost from AWS
+    public static String getInstantAWSCost(String instanceType, ProfileCredentialsProvider credentialsProvider, PricingClient pricingClient, Scanner input) throws StringIndexOutOfBoundsException, Exception
+    {
+            GetProductsRequest request = GetProductsRequest.builder().serviceCode("AmazonEC2")
+                    .filters(Filter.builder().field("instanceType").value(instanceType)
+                            .type("TERM_MATCH").build()).maxResults(1).build();
+
+            GetProductsResponse response = pricingClient.getProducts(request);
+            // Get price
+            String resp = response.toString();
+            String terms = resp.substring(resp.indexOf("terms"));
+            String price = terms.substring(terms.indexOf("pricePerUnit"));
+            return price.substring(14, price.indexOf("}") + 1);
+    }
 
 }
