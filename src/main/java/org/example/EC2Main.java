521be5e38e67568d68ea5fb9fa5efd4d8a76fd0d
compi314159 <71290813+compi314159@users.noreply.github.com>
2023-08-02 12:05:55 -0500

    v3
    Gets instance type price from recent json file

--- src/main/java/org/example/EC2Main.java
+++ src/main/java/org/example/EC2Main.java
@@ -5,35 +5,38 @@ import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import java.io.*;
 import java.net.URL;
-import java.net.URLConnection;
-import java.util.Map;
+
 import java.util.Properties;
 import java.util.Scanner;
 import software.amazon.awssdk.services.pricing.PricingClient;
 import software.amazon.awssdk.services.pricing.model.*;
 import redis.clients.jedis.Jedis;
-//
+
 import java.io.BufferedReader;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-//
-public class EC2Main {
-    public static void main(String[] args) throws IOException {
+import java.util.Map;
+import java.util.Map.Entry;
+
+
+public class EC2Main
+{
+    public static boolean found = false;
+    public static void main(String[] args) throws IOException
+    {
         // read properties
         Properties prop = new Properties();
         File configuration = new File("src/main/resources/config.properties");
         prop.load(new FileInputStream(configuration));
 
         // connect to jedis
-        Jedis jedis = new Jedis("redis://" + prop.getProperty("redisUser")+":" + prop.getProperty("redisPass")
-                + "@" + prop.getProperty("redisPublicEndpoint"));
+        Jedis jedis = new Jedis("redis://" + prop.getProperty("redisUser").trim() + ":" + prop.getProperty("redisPass").trim()
+                + "@" + prop.getProperty("redisPublicEndpoint").trim());
         jedis.connect();
 
         // check if connection is successful
-        if(jedis.ping().equalsIgnoreCase("pong"))
+        if (jedis.ping().equalsIgnoreCase("pong"))
         {
             System.out.println("Connection successful");
         }
@@ -45,91 +48,194 @@ public class EC2Main {
                 .credentialsProvider(credentialsProvider).build();
 
         System.out.println("\n*** EC2 Instance Type Price Calculator ***");
+
+
         String cont = "y";
-        while(cont.equalsIgnoreCase("y")) {
+        while (cont.equalsIgnoreCase("y"))
+        {
             System.out.print("\nEnter EC2 instance type: ");
             String instanceType = input.nextLine().trim();
+            System.out.println("\nOperating Systems: ");
+            System.out.println("\t1 - Windows");
+                System.out.println("\t2 - RHEL");
+                System.out.println("\t3 - SUSE");
+                System.out.println("\t4 - Linux");
+                System.out.println("\t5 - Red Hat Enterprise Linux with HA");
+                System.out.println("\t6 - Ubuntu Pro");
+                System.out.print("Select operating system: ");
+                String selection = input.nextLine();
+                int choice;
+                try
+                {
+                    choice = Integer.parseInt(selection);
+                }
+                catch(Exception e)
+                {
+                    choice = 0;
+                }
+                String os = "";
+                if(choice == 1)
+                {
+                    os = "Windows";
+                }
+                else if(choice == 2)
+                {
+                    os = "RHEL";
+                }
+                else if(choice == 3)
+                {
+                    os = "SUSE";
+                }
+                else if(choice == 4)
+                {
+                    os = "Linux";
+                }
+                else if(choice == 5)
+                {
+                    os = "Red Hat Enterprise Linux with HA";
+                }
+                else if(choice == 6)
+                {
+                    os = "Ubuntu Pro";
+                }
+                else
+                {
+                    System.out.println("Invalid entry, operating system defaulted to Windows");
+                    os = "Windows";
+                }
+
             try
             {
-                // json work starts here
-
-                URL json = new URL("https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/AmazonEC2/current/us-east-1/index.json");
-                ObjectMapper mapper = new ObjectMapper();
-                Map<String,Object> map = mapper.readValue(json, Map.class);
-                JSONObject job = new JSONObject(map);
-                parse(job.toString());
-                System.out.println(job.get("version"));
-
-//                String strJson = getJSONFromURL("https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/AmazonEC2/current/us-east-1/index.json");
-//                try {
-//                    JSONParser parser = new JSONParser();
-//                    Object object = parser.parse(strJson);
-//                    JSONObject mainJsonObject = (JSONObject) object;
-//
-//                    /*************** First Name ****************/
-//                    String firstName = (String) mainJsonObject.get("formatVersion");
-//                    System.out.println("First Name : " + 1);
-//                    }
-//
-//                catch(Exception ex) {
-//                    ex.printStackTrace();
-//                }
-
-
-
-            //String jsonString = stream(json);
-                //jsonString = jsonString.substring(jsonString.indexOf(instanceType),jsonString.indexOf(instanceType)+10);
-                //System.out.println(jsonString);
-               /* JSONArray jsonArr = new JSONArray(data);
-
-                JSONObject jsnobject = new JSONObject(readlocationFeed);
-
-                JSONArray jsonArray = jsnobject.getJSONArray("locations");
-                for (int i = 0; i < jsonArray.length(); i++) {
-                    JSONObject explrObject = jsonArray.getJSONObject(i);
-                }*/
-                // json work ends here
                 String price;
-                if(!(getAWSCost(jedis, instanceType) == null))
+                if (!(getAWSCost(jedis, instanceType, os) == null))
                 {
-                    price = getAWSCost(jedis, instanceType);
-                    System.out.println(instanceType + " successfully found in database!");
+                    price = getAWSCost(jedis, instanceType, os);
+                    System.out.println("Successfully found in database!");
                 }
-                else {
-                    System.out.println(instanceType + " not found in database. Receiving from aws");
-                    price = getInstantAWSCost(instanceType, credentialsProvider, pricingClient, input);
-                    addAWSCost(jedis, instanceType, price);
+                else
+                {
+                    System.out.println("\nNot found in database. Receiving from json file.");
+                    price = getJSONCost(instanceType, os);
+                    if (!found)
+                    {
+                        URL EC2Types = new URL("https://aws.amazon.com/ec2/instance-types/");
+                        System.out.println("Instance type with given operating system does not exist, please enter valid instance type.");
+                        System.out.println("Also make sure it exists with selected operating system (see " + EC2Types + ").");
+                    }
+                    addAWSCost(jedis, instanceType, os, price);
                 }
-                System.out.println("--> " + instanceType + " price per hour: " + price);
+                System.out.println("--> " + instanceType + " with " + os + " operating system price per hour: $" + price);
                 System.out.print("Continue? (y/n): ");
                 cont = input.nextLine();
             }
-            catch (StringIndexOutOfBoundsException e)
+            catch (ParseException e)
+            {
+                throw new RuntimeException(e);
+            }
+            catch (Exception e)
             {
                 System.out.println("Error: " + e);
-                URL EC2Types = new URL("https://aws.amazon.com/ec2/instance-types/");
-                System.out.println("Please enter valid instance type (see " + EC2Types + ").");
-            }
-            catch (Exception e) {
-                System.out.println("Error: " + e);
-                System.out.println("Invalid entry, please try again.");
+                input.nextLine();
+                System.out.print("Invalid entry, try again? (y/n): ");
+                cont = input.nextLine();
             }
         }
     }
-
     // add cost to database
-    private static void addAWSCost(Jedis jedis, String instanceType, String price) {
-        jedis.set(instanceType, price);
-    }
+    private static void addAWSCost(Jedis jedis, String instanceType, String os, String price) {
+    jedis.set(instanceType+","+os, price);
+}
 
     // get cost from database
-    public static String getAWSCost(Jedis jedis, String instanceType)
+    public static String getAWSCost(Jedis jedis, String instanceType, String os)
     {
-        return jedis.get(instanceType);
+        return jedis.get(instanceType+","+os);
+    }
+    // get cost from json file
+    public static String getJSONCost(String instanceType, String os) throws IOException, ParseException {
+        downloadFile(); // call method to download json file
+
+        Object obj = new JSONParser().parse(new FileReader("index.json"));
+        JSONObject json = (JSONObject) obj;
+        JSONObject products = (JSONObject) json.get("products");
+        System.out.println("\nLoading...products.toString() in progress");
+        Map<String, Object> map2 = new ObjectMapper().readValue(products.toString(), Map.class);
+        System.out.println("Loading...searching for instance type");
+        String sku = "";
+        for (Entry<String, Object> entry : map2.entrySet())
+        {
+            sku = entry.getKey();
+            Object value = entry.getValue();
+            if(value.toString().contains(instanceType) && value.toString().contains
+                    ("Shared") && value.toString().contains(os))
+            {
+                found = true;
+            }
+            if(found)
+            {
+                break;
+            }
+        }
+
+        JSONObject terms = (JSONObject) json.get("terms");
+        JSONObject onDemand = (JSONObject) terms.get("OnDemand");
+        return getPriceWithSKU(onDemand, sku);
+    }
+    // search second part of json file using sku for price
+    private static String getPriceWithSKU(JSONObject onDemand, String sku) {
+        JSONObject sku1 = (JSONObject) onDemand.get(sku);
+        JSONObject sku2 = (JSONObject) sku1.get(sku+".JRTCKXETXF");
+        JSONObject priceDimensions = (JSONObject) sku2.get("priceDimensions");
+        JSONObject priceDimensions2 = (JSONObject) priceDimensions.get(sku+".JRTCKXETXF" + ".6YS6EN2CT7");
+        JSONObject price1 = (JSONObject) priceDimensions2.get("pricePerUnit");
+        String price2 = (String) price1.get("USD");
+        return price2;
+    }
+    // download json file
+    private static void downloadFile() throws IOException {
+        File f = new File("index.json");
+        System.out.print("JSON file download status: ");
+        if (!f.exists() || f.isDirectory()) {
+            URL url = new URL("https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/AmazonEC2/current/us-east-1/index.json");
+            BufferedReader readr =
+                    new BufferedReader(new InputStreamReader(url.openStream()));
+
+            // Enter filename in which you want to download
+            BufferedWriter writer =
+                    new BufferedWriter(new FileWriter("index.json"));
+
+            // read each line from stream until end
+            String line;
+            while ((line = readr.readLine()) != null) {
+                writer.write(line);
+            }
+            readr.close();
+            writer.close();
+            System.out.println("JSON file successfully downloaded.");
+        } else {
+            System.out.println("JSON file already downloaded.");
+        }
     }
 
-    // get cost from AWS
-    public static String getInstantAWSCost(String instanceType, ProfileCredentialsProvider credentialsProvider, PricingClient pricingClient, Scanner input) throws StringIndexOutOfBoundsException, Exception
+    private static void createFile(JSONObject instances) throws IOException {
+        File f = new File("new.json");
+        System.out.print("\nJSON file download status: ");
+        if (!f.exists() || f.isDirectory()) 
+        {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("new.json"));
+            writer.write(instances.toString());
+            writer.close();
+            System.out.println("new JSON file successfully created.");
+        } 
+        else 
+        {
+            System.out.println("new JSON file already created.");
+        }
+    }
+
+
+    // get cost from AWS - no longer used - incorrect prices
+    public static String getInstantAWSCost(String instanceType, PricingClient pricingClient)
     {
             GetProductsRequest request = GetProductsRequest.builder().serviceCode("AmazonEC2")
                     .filters(Filter.builder().field("instanceType").value(instanceType)
@@ -142,79 +248,5 @@ public class EC2Main {
             String terms = resp.substring(resp.indexOf("terms"));
             String price = terms.substring(terms.indexOf("pricePerUnit"));
             return price.substring(14, price.indexOf("}") + 1);
-    }
-    public static String stream(URL url) {
-        try (InputStream input = url.openStream()) {
-            InputStreamReader isr = new InputStreamReader(input);
-            BufferedReader reader = new BufferedReader(isr);
-            StringBuilder json = new StringBuilder();
-            int c;
-            while ((c = reader.read()) != -1) {
-                json.append((char) c);
-            }
-            return json.toString();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    public static String getJSONFromURL(String strUrl) {
-        String jsonText = "";
-
-        try {
-            URL url = new URL(strUrl);
-            InputStream is = url.openStream();
-
-            BufferedReader bufferedReader =
-                    new BufferedReader(new InputStreamReader(is));
-
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                jsonText += line + "\n";
-            }
-
-            is.close();
-            bufferedReader.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-
-        return jsonText;
-    }
-    public static void parse(String string)
-    {
-        JSONParser parser = new JSONParser();
-
-        try {
-            URL oracle = new URL("://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/AmazonEC2/current/us-east-1/index.json"); // URL to Parse
-
-            URLConnection yc = oracle.openConnection();
-            BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
-
-            String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-                JSONArray a = (JSONArray) parser.parse(inputLine);
-
-                // Loop through each item
-                for (Object o : a) {
-                    JSONObject tutorials = (JSONObject) o;
-
-                    Long id = (Long) tutorials.get("sku");
-                    System.out.println("Post ID : " + id);
-
-                    String title = (String) tutorials.get("productFamily");
-                    System.out.println("Post Title : " + title);
-
-                    System.out.println("\n");
-                }
-            }
-            in.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
     }
 }
